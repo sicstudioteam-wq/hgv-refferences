22dc5332bf9b6bfeef468f7e7610ebfee1c82bbb
armcraft <sicstudioteam@gmail.com>
2026-09-20 16:54:27 +0800

    Add Vite config for GAiS preview

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  appType: 'mpa',
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: false,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    strictPort: false,
+  },
+});
